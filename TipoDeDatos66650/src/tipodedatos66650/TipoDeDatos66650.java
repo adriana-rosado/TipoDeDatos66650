6c1c927d6cbf5021b9653cfe76bd1f078d61c7fa
Sonia <Sonia@DESKTOP-3DOB9FH.huawei.net>
2021-03-05 08:45:11 -0600

    Tipos de datos 1

---
+++ TipoDeDatos66650/src/tipodedatos66650/TipoDeDatos66650.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos66650;
+
+/**
+ *
+ * @author Adriana
+ */
+public class TipoDeDatos66650 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Valor mínimo de Byte= "+ Byte.MIN_VALUE);
+        System.out.println("Valor máximo de Byte= "+ Byte.MAX_VALUE);
+        System.out.println("Valor Mínimo de Int= "+ Integer.MIN_VALUE);
+        System.out.println("Valor Máximo de Int= "+ Integer.MAX_VALUE);
+        System.out.println("Valor Mínimo de Short= "+ Short.MIN_VALUE);
+        System.out.println("Valor Máximo de Short= "+ Short.MAX_VALUE);
+        System.out.println("Valor Minimo de Double= "+ Double.MIN_VALUE);
+        System.out.println("Valor Máximo de Double= "+ Double.MAX_VALUE);
+        System.out.println("Valor Mínimo de Long= "+ Long.MIN_VALUE);
+        System.out.println("Valor Máximo de Long= "+ Long.MAX_VALUE);
+     
+    }
+    
+}
